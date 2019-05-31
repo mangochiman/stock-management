3332ea2d15013a579d55f8e85370a8c0e4959c8c
Ernest Matola <mangochiman@gmail.com>
2019-05-31 11:19:58 +0200

    UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def bar_name
+    settings = YAML.load_file(Rails.root.to_s + "/config/settings.yml")
+    bar_name = settings["bar_name"]
+    return bar_name
+  end
 end
